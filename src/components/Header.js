13d7d9f9d4d97e44e9fb1b62b00ceaff53391deb
9gl <glunkad007@gmail.com>
2022-08-24 22:30:15 +0530

    header component created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="images/troll-face.png" />
+            <h1 className="header--title">Header Component</h1>
+            <h4 className="header--project">Meme Generator</h4>
+        </header>
+    )
+}
